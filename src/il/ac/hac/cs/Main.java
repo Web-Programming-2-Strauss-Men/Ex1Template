250ea4eb047b91c65be8200b884506bf42636d38
Hezi <yhirshhorn@gmail.com>
2022-03-07 13:53:29 +0200

    Added Jsoup via maven

--- src/il/ac/hac/cs/Main.java
+++ src/il/ac/hac/cs/Main.java
@@ -1,5 +1,11 @@
 package il.ac.hac.cs;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
